3c89792e5b1afb9a6e941b5c87b17c82645d0a04
SungtaeMoon <yojunavi@gmail.com>
2020-12-09 19:04:32 +0900

    24강. 검색서비스 추가하기

--- JDBCPrj/src/com/newlecture/app/service/NoticeService.java
+++ JDBCPrj/src/com/newlecture/app/service/NoticeService.java
@@ -18,17 +18,19 @@ public class NoticeService {
 	private String pwd    = "kikja";
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 
-	public List<Notice> getList(int page) throws ClassNotFoundException, SQLException {
+	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
+		
 		int start = 1 + (page-1)*10; //1, 11, 21, 31, ...
 		int end   = 10 * page;       //10, 20, 30, 40, ...
 		
-		String sql = "select * from  notice_view where num between ? and ?";
+		String sql = "select * from  notice_view where "+field+" like ? and num between ? and ?";
 		
 		Class.forName(driver);
 		Connection        con = DriverManager.getConnection(url, uid, pwd);
 		PreparedStatement st  = con.prepareStatement(sql);
-		st.setInt(1, start);
-		st.setInt(2, end);
+		st.setString(1,  "%"+query+"%");;
+		st.setInt(2, start);
+		st.setInt(3, end);
 		ResultSet rs = st.executeQuery();
 		
 		List<Notice> list = new ArrayList<Notice>();
